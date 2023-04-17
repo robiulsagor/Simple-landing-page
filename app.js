e9be651288e8b7805083d62f93b26e5e6adcbcf2
Robiul Islam Sagar <robiul100.me@gmail.com>
2023-04-17 10:50:36 +0600

    Fixed issues

--- app.js
+++ app.js
@@ -1,15 +1,8 @@
 const toggleBtn = document.querySelectorAll(".footer span")
-
 const body = document.querySelector(".big-wrapper")
-const overlay = document.querySelector(".overlay")
-
 var dark = false
-
-
 toggleBtn.forEach(btn => {
     btn.addEventListener("click", () => {
-        overlay.classList.add("show")
-        overlay.classList.remove("hide")
         if (!dark) {
             body.classList.remove("light")
             body.classList.add("dark")
@@ -19,10 +12,5 @@ toggleBtn.forEach(btn => {
             body.classList.add("light")
             dark = false
         }
-        overlay.addEventListener("animationend", () => {
-            overlay.classList.remove("show")
-            overlay.classList.add("hide")
-        })
     })
 })
-console.log(toggleBtn);
