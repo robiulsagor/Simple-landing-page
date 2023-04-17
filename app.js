466b548d3d6ce52ccaeb92d78e5ba579ef245dad
Robiul Islam Sagar <robiul100.me@gmail.com>
2023-04-17 12:03:27 +0600

    Solved

--- app.js
+++ app.js
@@ -1,30 +1,24 @@
 const toggleBtn = document.querySelectorAll(".footer span")
 const body = document.querySelector(".big-wrapper")
-<<<<<<< HEAD
-var dark = false
-toggleBtn.forEach(btn => {
-    btn.addEventListener("click", () => {
-        if (!dark) {
-=======
 
 var dark = false
 toggleBtn.forEach(btn => {
     btn.addEventListener("click", () => {
-              if (!dark) {
->>>>>>> 101c074f059dd359fd134f338f61cfdaa3152616
-            body.classList.remove("light")
-            body.classList.add("dark")
-            dark = true
-        } else {
-            body.classList.remove("dark")
-            body.classList.add("light")
-            dark = false
-        }
-<<<<<<< HEAD
+
+        var dark = false
+        toggleBtn.forEach(btn => {
+            btn.addEventListener("click", () => {
+                if (!dark) {
+                    body.classList.remove("light")
+                    body.classList.add("dark")
+                    dark = true
+                } else {
+                    body.classList.remove("dark")
+                    body.classList.add("light")
+                    dark = false
+                }
+            })
+        })
+
     })
 })
-=======
-        
-    })
-})
->>>>>>> 101c074f059dd359fd134f338f61cfdaa3152616
